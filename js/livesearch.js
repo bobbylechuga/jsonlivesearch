9695efd5dceded0d71020078954372ebe3f98c4d
José Pino <jose.pino@latercara.com>
2016-10-18 17:19:47 -0300

    mejoradas busquedas para buscar por nombres, apellidos, partes del nombre, del apellido, etc2

--- js/livesearch.js
+++ js/livesearch.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+  //php_vars.home Es una vairable de wordpress invocada en el function - wp_localize_script -
+
+
   $("#live-search").on("submit", function(e){
       e.preventDefault();
   });
-  $.getJSON( "candidatos.json", function( data ) {
+  $.getJSON("candidatos.json", function( data ) {
     $.each( data, function( clave, valor ) {
       if (clave <= 10) {
         listaDeCandidatos(valor);
@@ -10,14 +13,23 @@ $(document).ready(function(){
     });
     $('#filter').keyup(function(){
       var inputCandidato = $('#filter').val();
-      var regex = new RegExp(inputCandidato, "i");
 
-      if (inputCandidato.length > 4) {
+      if (inputCandidato.length > 3) {
+        var inputCandidatoPartes = inputCandidato.split(' ');
         $('.lista-candidaatos li').remove();
         $.each(data, function(key, val){
-          if ((val.value.search(regex) != -1)) {
+          var contcandidato = 0;
+          for(var k=0; k < inputCandidatoPartes.length; k++) {
+            var regex = new RegExp(inputCandidatoPartes[k], "i");
+            //console.log(inputCandidatoPartes[k]);
+            if ((val.value.search(regex) != -1)) {
+              //listaDeCandidatos(val);
+              //console.log(val.value);
+              contcandidato++;
+            }
+          }
+          if (contcandidato == inputCandidatoPartes.length) {
             listaDeCandidatos(val);
-            //console.log(val.value);
           }
         });
         if ( $('.lista-candidaatos li').length == 0 ) {
@@ -35,6 +47,6 @@ $(document).ready(function(){
   });
 
   function listaDeCandidatos(valor) {
-    $('.lista-candidaatos').append('<li><a href="'+valor.url+'">'+valor.value+' - '+valor.cargo+' - <span class"busqueda-comuna">'+valor.comuna+'</span></a></li>');
+    $('.lista-candidaatos').append('<li><a href="'+valor.url+'"><strong>'+valor.value+'</strong> - '+valor.cargo+' - <span class"busqueda-comuna">'+valor.comuna+'</span></a></li>');
   }
 });
